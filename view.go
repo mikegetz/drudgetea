8259fc11d907c114244aa86b3553a63bba2eacea
mguitar24 <mguitar24@gmail.com>
2026-02-22 13:04:45 -0700

    refactor view, easier to read and reason

--- view.go
+++ view.go
@@ -8,56 +8,27 @@ import (
 	"github.com/charmbracelet/lipgloss"
 )
 
-func (m model) View() string {
-	cs := " | "
-	columnSeparator := lipgloss.NewStyle().Foreground(lipgloss.Color("#888888")).Render(cs)
-	centerStyle := lipgloss.NewStyle().Width(m.width).Align(lipgloss.Center)
-	redStyle := lipgloss.NewStyle().Foreground(lipgloss.Color("#FF0000"))
-	blueStyle := lipgloss.NewStyle().Foreground(lipgloss.Color("#3030fe"))
+var (
+	cs              = " | "
+	columnSeparator = lipgloss.NewStyle().Foreground(lipgloss.Color("#888888")).Render(cs)
+	redStyle        = lipgloss.NewStyle().Foreground(lipgloss.Color("#FF0000"))
+	blueStyle       = lipgloss.NewStyle().Foreground(lipgloss.Color("#3030fe"))
+)
 
+func (m model) View() string {
+	centerStyle := lipgloss.NewStyle().Width(m.width).Align(lipgloss.Center)
 	view := centerStyle.Render(logo)
 
 	view += "\n"
 
-	columnWidth := (m.width - (len(cs) * 2)) / len(m.headlines)
-
-	if columnWidth > 3 {
-		for i := 0; i < m.maxRows; i++ {
-			for j, col := range m.headlines {
-				if i < len(col) {
-					var headlineStyle lipgloss.Style
-					if col[i].Color == "\033[31m" {
-						headlineStyle = redStyle.Width(columnWidth)
-					} else {
-						headlineStyle = blueStyle.Width(columnWidth)
-					}
-					if i == m.cursory && j == m.cursorx {
-						m.selected = col[i]
-						headlineStyle = headlineStyle.Bold(true)
-					}
-
-					if len(col[i].Title) > columnWidth {
-						view += headlineStyle.Render(col[i].Title[:columnWidth-3] + "...")
-					} else {
-						view += headlineStyle.Render(col[i].Title)
-					}
-				} else {
-					view += strings.Repeat(" ", columnWidth)
-				}
-				if j < len(m.headlines)-1 {
-					view += columnSeparator
-				}
-			}
-			view += "\n"
-		}
-	}
+	view += m.ColumnView()
 
 	// The footer
 
 	view += "\n Selected: " + m.selected.Title + "\n(" + m.selected.Href + ")\n"
 
 	if os.Getenv("DEBUG") != "" {
-		view += m.inputStyle.Render("Debug: "+fmt.Sprintf("cursorx: %d, cursory: %d, curMaxRow: %d, columnWidth: %d", m.cursorx, m.cursory, m.curMaxRow, columnWidth)) + "\n"
+		view += m.inputStyle.Render("Debug: "+fmt.Sprintf("cursorx: %d, cursory: %d, curMaxRow: %d, columnWidth: %d", m.cursorx, m.cursory, m.curMaxRow, m.columnWidth)) + "\n"
 	}
 
 	// The help view
@@ -67,5 +38,41 @@ func (m model) View() string {
 	view += strings.Repeat("\n", height) + helpView
 
 	// Send the UI for rendering
+	return view
+}
+
+func (m *model) ColumnView() string {
+	m.columnWidth = (m.width - (len(cs) * 2)) / len(m.headlines)
+	view := ""
+	if m.columnWidth > 3 {
+		for i := 0; i < m.maxRows; i++ {
+			for j, col := range m.headlines {
+				if i < len(col) {
+					var headlineStyle lipgloss.Style
+					if col[i].Color == "\033[31m" {
+						headlineStyle = redStyle.Width(m.columnWidth)
+					} else {
+						headlineStyle = blueStyle.Width(m.columnWidth)
+					}
+					if i == m.cursory && j == m.cursorx {
+						m.selected = col[i]
+						headlineStyle = headlineStyle.Bold(true)
+					}
+
+					if len(col[i].Title) > m.columnWidth {
+						view += headlineStyle.Render(col[i].Title[:m.columnWidth-3] + "...")
+					} else {
+						view += headlineStyle.Render(col[i].Title)
+					}
+				} else {
+					view += strings.Repeat(" ", m.columnWidth)
+				}
+				if j < len(m.headlines)-1 {
+					view += columnSeparator
+				}
+			}
+			view += "\n"
+		}
+	}
 	return view
 }
